0c94af20bf80b7de4ce8754322590bea7e330145
SiddhartaDutta <siddhartadutta99@gmail.com>
2023-07-11 09:49:40 -0700

    Sort by provided columns but return full row

--- test.py
+++ test.py
@@ -1,52 +1,34 @@
 import os
 import csv
 import boto3
+import logging
 import pandas as pd
 from dotenv import load_dotenv
 
 load_dotenv()
 
 s3 = boto3.client('s3')
+sns_client = boto3.client('sns')
 
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
+# test_data > diff folder
+# test_upload > incoming data
+
+BUCKET_NAME = os.environ.get('BUCKET_NAME')
 OBJECT_NAME = 'diff.csv'
 
-srcFile = os.environ.get("SRC_FILE")
-newFile = os.environ.get("NEW_FILE")
+TOPIC_ARN = os.environ.get('TOPIC_ARN')
+
+srcFile = 's3://' + BUCKET_NAME + '/test_upload/src.csv'
+newFile = 's3://' + BUCKET_NAME + '/test_upload/new.csv'
+
+srcFile = 'src.csv'
+newFile = 'new.csv'
+
+print(srcFile)
+print(newFile)
 
 colName = ['id', 'first_name', 'address']
 delimiter = ','
-
-
-def main():
-
-    # Read .csv files
-    src = pd.read_csv(srcFile, usecols= colName, sep= delimiter)
-    new = pd.read_csv(newFile, usecols= colName, sep= delimiter)
-
-    # Get differences
-    diff_in_new = new[~new.apply(tuple,1).isin(src.apply(tuple,1))]
-
-    # Write new records into .csv file
-    with open('diff.csv', 'a') as diff:
-        # Get cols
-        cols = diff_in_new.head()
-
-        # Write to file 
-            # Create writer
-        writer = csv.writer(diff)
-
-            # Write header
-        writer.writerow(cols)
-
-            # Write rows
-        for row in range(len(diff_in_new.index)):
-            writer.writerow(diff_in_new.iloc[row])
-
-    # Upload to AWS
-    with open("diff.csv", "rb") as f:
-        s3.upload_fileobj(f, BUCKET_NAME, OBJECT_NAME)
-
 
 # Print changes
 def printAllChanges(diff_in_orig, diff_in_new):
@@ -56,6 +38,74 @@ def printAllChanges(diff_in_orig, diff_in_new):
                 print('id: ' + str(diff_in_orig.at[diff_in_orig.index[entry], 'id']))
                 print('old: ' + str(diff_in_orig.at[diff_in_orig.index[entry], col]))
                 print('new: ' + str(diff_in_new.at[diff_in_new.index[entry], col]) + '\n')
+
+# Send email
+def sendEmail(snsClient, subject, message, topicArn):
+    snsClient.publish(TopicArn= topicArn,
+                   Message= message,
+                   Subject = subject)
+
+# Create pre-assigned URL
+def createPreassignedURL(s3Client, bucket, object, expiration):
+    try:
+        response = s3Client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket,
+                                                            'Key': object},
+                                                    ExpiresIn=expiration)
+
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    return response
+
+# MAIN
+def main():
+
+    # Read full-> Create and compare short 
+    try:
+        src = pd.read_csv(srcFile, usecols= colName, sep= delimiter)
+        #src = srcFull[colName]
+    except:
+        print('Missing src file')
+        sendEmail(sns_client, 'Daily Check: Failed', 'Daily check could not be completed: missing \'src.csv\' file.', TOPIC_ARN)
+        exit()
+        
+    try:
+        newFull = pd.read_csv(newFile, sep= delimiter)
+        new = newFull[colName]
+    except:
+        print('Missing new file')
+        sendEmail(sns_client, 'Daily Check: Failed', 'Daily check could not be completed: missing \'new.csv\' file.', TOPIC_ARN)
+        exit()
+
+    # Get differences
+    diff_in_new = new[~new.apply(tuple,1).isin(src.apply(tuple,1))]
+
+    # Store IDs
+    diffIDS = diff_in_new['id']
+
+    # Cut down new
+    newFull = newFull[newFull['id'].isin(diffIDS)]
+    
+    if len(diff_in_new):
+
+        # Write new records into .csv file
+        with open('diff.csv', 'w') as diff:
+            newFull.to_csv('diff.csv', index= False)
+    
+        # Upload to AWS
+        with open("diff.csv", "rb") as f:
+            s3.upload_fileobj(f, BUCKET_NAME, 'test_data/' + OBJECT_NAME)
+            
+        print('Changes Found')
+        sendEmail(sns_client, 'Daily Check',
+                    'Difference found between daily files. diff.csv uploaded to \'test_data\'. Download link: ' + createPreassignedURL(s3, BUCKET_NAME, 'test_data/' + OBJECT_NAME, 3600),
+                    TOPIC_ARN)
+        
+    else:
+        print('No Changes Found')
+        sendEmail(sns_client, 'Daily Check', 'No difference found between files; no uploaded difference file.', TOPIC_ARN)
 
 if __name__ == "__main__":
     main()
